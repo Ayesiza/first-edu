7a14cc68b5ee8f986a5322c6e36c2105aeec1f5d
Ayesiza Hawah <yesiha2001@gmail.com>
2020-05-02 21:40:01 +0300

    create components

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -5,26 +5,26 @@ class Navbar extends Component {
     render() { 
         return ( 
             <div>
-             <nav class="  navbar navbar-expand-lg navbar-light bg-secondary ">
-        <a style={{color:'orange'}} class="navbar-brand " href="">Hi-Five Designers</a>
-        <button class="navbar-toggler" type="button">
-          <span class="navbar-toggler-icon"></span>
+             <nav className="  navbar navbar-expand-lg navbar-light bg-secondary ">
+        <a style={{color:'orange'}} className="navbar-brand " href="">Hi-Five Designers</a>
+        <button className="navbar-toggler" type="button">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" >
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+        <div className="collapse navbar-collapse" >
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
             <button className="btn btn-secondary btn-m m-2 ">Home</button>
              
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
             <button className="btn btn-secondary btn-m m-2 ">Link</button>
             </li>
            
            
           </ul>
-          <ul class="navbar-nav ml-auto">
-          <li class="nav-item ">
+          <ul className="navbar-nav ml-auto">
+          <li className="nav-item ">
           <button className="btn btn-secondary btn-m m-2 ">Signup</button>
           {/* <a class="nav-link" href="#">Signup</a> */}
           </li>
